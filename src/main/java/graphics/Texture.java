1e626182a87d7b1e427f9e544205904a7dc7bad8
Márton Bán <30813844+martonban@users.noreply.github.com>
2024-03-02 23:07:25 +0100

    Texture init

---
+++ src/main/java/graphics/Texture.java
@@ -0,0 +1,14 @@
+package graphics;
+
+public class Texture {
+    private int width, height;
+    private int texture;
+
+    public Texture(String path) {
+        texture = load(path);
+    }
+
+    private int load(String path) {
+        
+    }
+}
